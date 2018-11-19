94a109e4c88013b5654780e92bb53d704dd5891a
张翼 <zhangyi19941217@gmail.com>
2018-11-19 15:53:15 +0800

    Update multi_client.py

--- multi_client.py
+++ multi_client.py
@@ -10,7 +10,7 @@ def start_client():
 
 if __name__ == '__main__':
     jobs = []
-    for i in range(10):
+    for i in range(8):
         # threading.Thread(target=start_client).start()
         print("start client" + str(i))
         fo_name = "timeline_clinet" + str(i) + ".txt"
