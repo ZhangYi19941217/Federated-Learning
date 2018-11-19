c956ebb5c7ea75b8ac6d2af32ee2131460cc9675
张翼 <zhangyi19941217@gmail.com>
2018-11-19 17:10:50 +0800

    Update multi_client.py

--- multi_client.py
+++ multi_client.py
@@ -5,8 +5,8 @@ import multiprocessing
 import threading
 import time
 
-def start_client():
-    c = FL_run()
+def start_client(fo_name, f_training_name):
+    c = FederatedClient("172.31.14.70", 5000, datasource.Mnist, fo_name, f_training_name)
 
 
 if __name__ == '__main__':
@@ -15,16 +15,9 @@ if __name__ == '__main__':
         # threading.Thread(target=start_client).start()
         print("start client" + str(i))
         fo_name = "timeline_clinet" + str(i) + ".txt"
-        f_training_name = "time_training" + str(i) + ".txt"
+        f_training_name = "time_training" + str(i) + ".txt" 
         
-        fo = open(fo_name, "w")
-        f_training = open(f_training_name, "w")   
-        
-        time_start = time.time()
-        print("------------------------------------------------time_start: ", time_start)
-        fo.write("time_start:    " + str(time_start) + "\n")
-        
-        p = multiprocessing.Process(target=start_client)
+        p = multiprocessing.Process(target=start_client, args=(fo_name,f_training_name))
         jobs.append(p)
         p.start()
         #time.sleep(1)
