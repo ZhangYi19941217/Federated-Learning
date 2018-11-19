f7027b14cc8893de6143f3d71234db6af1b5bca5
张翼 <zhangyi19941217@gmail.com>
2018-11-19 10:02:00 +0800

    Update multi_client.py

--- multi_client.py
+++ multi_client.py
@@ -2,18 +2,20 @@ from fl_client import FederatedClient
 import datasource
 import multiprocessing
 import threading
+import time
 
 def start_client():
     print("start client")
-    c = FederatedClient("127.0.0.1", 5000, datasource.Mnist)
+    c = FederatedClient("172.31.14.70", 5000, datasource.Mnist)
 
 
 if __name__ == '__main__':
     jobs = []
-    for i in range(5):
+    for i in range(10):
         # threading.Thread(target=start_client).start()
 
         p = multiprocessing.Process(target=start_client)
         jobs.append(p)
         p.start()
+        time.sleep(1)
     # TODO: randomly kill
