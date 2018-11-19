b0bdc7a016f7795b569f977215c0cb1e12cf5f70
张翼 <zhangyi19941217@gmail.com>
2018-11-19 15:49:06 +0800

    Update multi_client.py

--- multi_client.py
+++ multi_client.py
@@ -12,15 +12,19 @@ if __name__ == '__main__':
     jobs = []
     for i in range(10):
         # threading.Thread(target=start_client).start()
-        #print("start client")
-        #fo_name = "timeline_clinet" + str(i) + ".txt"
-        #_training_name = "time_training" + str(i) + ".txt"
-        #time_start = time.time()
-        #print("------------------------------------------------time_start: ", time_start)
-        #fo.write("time_start:    " + str(time_start) + "\n")
+        print("start client" + str(i))
+        fo_name = "timeline_clinet" + str(i) + ".txt"
+        f_training_name = "time_training" + str(i) + ".txt"
+        
+        fo = open(fo_name, "w")
+        f_training = open(f_training_name, "w")   
+        
+        time_start = time.time()
+        print("------------------------------------------------time_start: ", time_start)
+        fo.write("time_start:    " + str(time_start) + "\n")
         
         p = multiprocessing.Process(target=start_client)
         jobs.append(p)
         p.start()
-        time.sleep(1)
+        #time.sleep(1)
     # TODO: randomly kill
