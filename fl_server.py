a81f684433b7940504134e695eb9308b7d48ad05
张翼 <zhangyi19941217@gmail.com>
2018-11-26 14:27:54 +0800

    Update fl_server.py

--- fl_server.py
+++ fl_server.py
@@ -147,21 +147,23 @@ class GlobalModel_MNIST_CNN(GlobalModel):
     def build_model(self):
         # ~?MB worth of parameters
         model = Sequential()
-        model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=(32,32,3),padding='same',activation='relu',kernel_initializer='he_normal'))
-        model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='he_normal'))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2), padding='same'))
+        model.add(Conv2D(32,(3,3),strides=(1,1),input_shape=(32,32,3),padding='same',activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
         
-        model.add(Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='he_normal'))
-        model.add(Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='he_normal'))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2), padding='same'))
+        model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
         
-        model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='he_normal'))
-        model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='he_normal'))
+        model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu'))
+        model.add(Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu'))
+        model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu'))
         model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
         
         model.add(Flatten())
+        model.add(Dense(1024, activation='relu')) 
         model.add(Dropout(0.5))
-        model.add(Dense(10,activation='softmax', kernel_initializer='he_normal'))
+        model.add(Dense(1024, activation='relu')) 
+        model.add(Dropout(0.5))
+        model.add(Dense(10,activation='softmax'))
 
         model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
         return model
