6ca85e7af8f9cf07b058f6ce7b683626f4707d67
张翼 <zhangyi19941217@gmail.com>
2018-11-27 10:28:07 +0800

    Update fl_server.py

--- fl_server.py
+++ fl_server.py
@@ -147,26 +147,29 @@ class GlobalModel_MNIST_CNN(GlobalModel):
     def build_model(self):
         # ~?MB worth of parameters
         model = Sequential()
-        model.add(Conv2D(32,(3,3),strides=(1,1),input_shape=(32,32,3),padding='same',activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+        model.add(Conv2D(96,(3,3),strides=(2,2),input_shape=(32,32,3),padding='same',activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2))) 
+        model.add(BatchNormalization())
         
-        model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+        model.add(Conv2D(256,(5,5),strides=(1,1),padding='same',activation='relu'))
+        model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+        model.add(BatchNormalization())
         
-        model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu'))
-        model.add(Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu'))
+        model.add(Conv2D(384,(3,3),strides=(1,1),padding='same',activation='relu'))
+        model.add(Conv2D(384,(3,3),strides=(1,1),padding='same',activation='relu'))
         model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+        model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+        model.add(BatchNormalization())
         
         model.add(Flatten())
-        model.add(Dense(2048, activation='relu')) 
+        model.add(Dense(4096, activation='tanh')) 
         model.add(Dropout(0.5))
-        model.add(Dense(1024, activation='relu')) 
+        model.add(Dense(4096, activation='tanh')) 
         model.add(Dropout(0.5))
         model.add(Dense(10,activation='softmax'))
 
-        model.compile(loss=keras.losses.categorical_crossentropy,
-                      optimizer=keras.optimizers.Adadelta(),
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=Adam(),
                       metrics=['accuracy'])
         
         return model
