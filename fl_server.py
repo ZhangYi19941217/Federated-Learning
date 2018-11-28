87542309dda49f8bdf4a1fdddd8cf7b9079385b9
张翼 <zhangyi19941217@gmail.com>
2018-11-28 13:56:18 +0800

    Update fl_server.py

--- fl_server.py
+++ fl_server.py
@@ -62,7 +62,7 @@ class GlobalModel(object):
                     total_size -= client_sizes[c]
                     break;
                 
-        print("------------------------------------------------------------------: " + type(str(client_weights[c][i])) )
+        print("------------------------------------------------------------------: " + str(type(client_weights[c][i])) )
         
         
         print("------------total_size=", total_size)
