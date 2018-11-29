ca1ac9493b80271534a4e2af2f8e5ea97ddb7745
张翼 <zhangyi19941217@gmail.com>
2018-11-29 19:02:18 +0800

    Update fl_server_new.py

--- fl_server_new.py
+++ fl_server_new.py
@@ -139,7 +139,9 @@ class GlobalModel_MNIST_CNN(GlobalModel):
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
         model.add(Flatten())
-        model.add(Dense(128, activation='relu')) 
+        model.add(Dense(1024, activation='relu')) 
+        model.add(Dropout(0.5))
+        model.add(Dense(512, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(10, activation='softmax'))
 
