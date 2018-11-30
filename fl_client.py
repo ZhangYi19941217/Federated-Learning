14844853c3f1a0bb1e016e5486b1b2a0af84600d
张翼 <zhangyi19941217@gmail.com>
2018-11-30 11:25:56 +0800

    Update fl_client.py

--- fl_client.py
+++ fl_client.py
@@ -281,5 +281,5 @@ if __name__ == "__main__":
     fo_name = "timeline_clinet.txt"
     f_training_name = "time_training.txt"
     
-    FederatedClient("172.31.14.70", 5000, datasource_CIFAR10.Cifar10, fo_name, f_training_name)
-    #FederatedClient("172.31.14.70", 5000, datasource.Mnist, fo_name, f_training_name)
+    #FederatedClient("172.31.20.224, datasource_CIFAR10.Cifar10, fo_name, f_training_name)
+    FederatedClient("172.31.20.224", 5000, datasource.Mnist, fo_name, f_training_name)
